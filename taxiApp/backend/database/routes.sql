fea4aca5cd8006727c3492b6e2b4e0cc02970587
sanele <sanele@projectcodex.co>
2014-12-09 17:04:27 +0200

    data setup

---
+++ taxiApp/backend/database/routes.sql
@@ -0,0 +1,7 @@
+INSERT INTO `routes` (`id`, `routeName`, `fare`, `geoLocationTripStart`, `geoLocationTripEnd`) VALUES
+(1, 'khayeltsha-capetown', 20, '', ''),
+(2, 'khayeltsha-Wynberg', 15, '', ''),
+(3, 'khayeltsha-Claremont', 13, '', ''),
+(4, 'khayeltsha-Paarl', 21, '', ''),
+(5, 'khayeltsha-Mobray', 16, '', '');
+
